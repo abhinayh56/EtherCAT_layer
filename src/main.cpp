1ee2be0ecd5327481f152ad4e1b7b2560e76d48d
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-11 11:38:45 +0530

    temp commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,28 +4,17 @@
 
 Ec_master ec_master;
 
-// int main()
-// {
-//     ec_master.start();
-
-//     ec_master.config();
-
-//     while (ec_master.is_running())
-//     {
-//         ec_master.cyclic_task();
-//         usleep(4000);
-//     }
-
-//     ec_master.stop();
-
-//     return 0;
-// }
-
-
 int main()
 {
     ec_master.start();
+
     ec_master.config();
+
+    while (ec_master.is_running())
+    {
+        ec_master.cyclic_task();
+        usleep(4000);
+    }
 
     ec_master.stop();
 
