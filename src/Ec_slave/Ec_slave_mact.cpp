94e8dcd61b412b866929ca4d8a83bae6bf58a075
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-12 18:30:14 +0530

    sdsgfdfg

--- src/Ec_slave/Ec_slave_mact.cpp
+++ src/Ec_slave/Ec_slave_mact.cpp
@@ -46,7 +46,7 @@ void Ec_slave_mact::transfer_tx_pdo()
     int32_t ACT_VEL = EC_READ_S32(domain_i_pd + off_tx_pdo_7);
     int16_t ADC_VAL = EC_READ_S16(domain_i_pd + off_tx_pdo_8);
 
-    std::cout << "MACT_TXPDO (addr): " << slave_address << ",  |  " << ACT_POS << ", " << STATUS_WD << ", " << ACT_TOR << ", " << uint16_t(OPMODE_DISP) << ", " << ERROR_CODE << ", " << uint16_t(DIG_IN) << ", " << ACT_VEL << ", " << ADC_VAL << std::endl;
+    std::cout << "MACT_TXPDO: " << slave_ns << " | " << ACT_POS << ", " << STATUS_WD << ", " << ACT_TOR << ", " << uint16_t(OPMODE_DISP) << ", " << ERROR_CODE << ", " << uint16_t(DIG_IN) << ", " << ACT_VEL << ", " << ADC_VAL << std::endl;
 }
 
 void Ec_slave_mact::transfer_rx_pdo()
