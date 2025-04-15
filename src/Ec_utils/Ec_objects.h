0572a501cd4530c7fedf3daaef353744f5b79e83
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-15 18:33:11 +0530

    asdfsdf

--- src/Ec_utils/Ec_objects.h
+++ src/Ec_utils/Ec_objects.h
@@ -8,11 +8,20 @@ namespace Object
     namespace Control_word
     {
         uint16_t index = 0x1600;
-        uint8_t sub_index = 0x01;
+        uint8_t sub_index = 0x00;
+        uint16_t pdo_index = 0x6040;
+        uint8_t pdo_sub_index = 0x00;
 
         enum Bit_flags : uint16_t
         {
-            
+            SHUTDOWN = (1<<2) | (1<<1),
+            SWITCH_ON = (1<<2) | (1<<1) | (1<<0),
+            SWITCH_ON_ENABLE_OPERATION = (1<<3) | (1<<2) | (1<<1) | (1<<0),
+            DISABLE_VOLTAGE = 0,
+            QUICK_STOP = 0,
+            DISABLE_OPERATION = 0,
+            ENABLE_OPERATION = 0,
+            FAULT_RESET = 0
         };
     }
 }
