11a20b745fb58c73d434df8e007d4bc7ade527aa
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-22 11:57:31 +0530

    sdfgfdg

--- src/Ec_slave_base_coupler/Ec_slave_base_coupler.h
+++ src/Ec_slave_base_coupler/Ec_slave_base_coupler.h
@@ -1,6 +1,13 @@
 #ifndef EC_SLAVE_BASE_COUPLER_H
 #define EC_SLAVE_BASE_COUPLER_H
 
+#include "../Ec_slave_base/Ec_slave_base.h"
 
+class Ec_slave_base_coupler : public Ec_slave_base
+{
+public:
+    Ec_slave_base_coupler();
+    ~Ec_slave_base_coupler();
+};
 
 #endif // EC_SLAVE_BASE_COUPLER_H
