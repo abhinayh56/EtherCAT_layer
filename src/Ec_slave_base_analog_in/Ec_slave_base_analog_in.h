b9ee897489a4a36c03b859b2e360cef004d58bdd
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-22 10:42:49 +0530

    asdfsdf

--- src/Ec_slave_base_analog_in/Ec_slave_base_analog_in.h
+++ src/Ec_slave_base_analog_in/Ec_slave_base_analog_in.h
@@ -1,4 +1,11 @@
 #ifndef EC_SLAVE_BASE_ANALOG_IN_H
 #define EC_SLAVE_BASE_ANALOG_IN_H
 
+class Ec_slave_base_analog_in
+{
+public:
+    Ec_slave_base_analog_in();
+    ~Ec_slave_base_analog_in();
+};
+
 #endif // EC_SLAVE_BASE_ANALOG_IN_H
