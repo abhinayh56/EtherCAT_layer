d49d62763ed0be9b854e006277e869a649e984b6
abhinayh56 <kumarabhinay53@gmail.com>
2025-04-22 11:32:46 +0530

    asdfsdf

--- src/Ec_slave_base_digital_in/Ec_slave_base_digital_in.h
+++ src/Ec_slave_base_digital_in/Ec_slave_base_digital_in.h
@@ -1,4 +1,45 @@
 #ifndef EC_SLAVE_BASE_DIGITAL_IN_H
 #define EC_SLAVE_BASE_DIGITAL_IN_H
 
+#include <stdint.h>
+
+namespace Digital_input
+{
+    enum Bit_flags : uint32_t
+    {
+        DIN_0 = 1u << 0,
+        DIN_1 = 1u << 1,
+        DIN_2 = 1u << 2,
+        DIN_3 = 1u << 3,
+        DIN_4 = 1u << 4,
+        DIN_5 = 1u << 5,
+        DIN_6 = 1u << 6,
+        DIN_7 = 1u << 7,
+        DIN_8 = 1u << 8,
+        DIN_9 = 1u << 9,
+        DIN_10 = 1u << 10,
+        DIN_11 = 1u << 11,
+        DIN_12 = 1u << 12,
+        DIN_13 = 1u << 13,
+        DIN_14 = 1u << 14,
+        DIN_15 = 1u << 15,
+        DIN_16 = 1u << 16,
+        DIN_17 = 1u << 17,
+        DIN_18 = 1u << 18,
+        DIN_19 = 1u << 19,
+        DIN_20 = 1u << 20,
+        DIN_21 = 1u << 21,
+        DIN_22 = 1u << 22,
+        DIN_23 = 1u << 23,
+        DIN_24 = 1u << 24,
+        DIN_25 = 1u << 25,
+        DIN_26 = 1u << 26,
+        DIN_27 = 1u << 27,
+        DIN_28 = 1u << 28,
+        DIN_29 = 1u << 29,
+        DIN_30 = 1u << 30,
+        DIN_31 = 1u << 31
+    };
+}
+
 #endif // EC_SLAVE_BASE_DIGITAL_IN_H
--- src/Ec_slave_base_digital_out/Ec_slave_base_digital_out.h
+++ src/Ec_slave_base_digital_out/Ec_slave_base_digital_out.h
@@ -1,4 +1,45 @@
 #ifndef EC_SLAVE_BASE_DIGITAL_OUT_H
 #define EC_SLAVE_BASE_DIGITAL_OUT_H
 
+#include <stdint.h>
+
+namespace Digital_input
+{
+    enum Bit_flags : uint32_t
+    {
+        DOUT_0 = 1u << 0,
+        DOUT_1 = 1u << 1,
+        DOUT_2 = 1u << 2,
+        DOUT_3 = 1u << 3,
+        DOUT_4 = 1u << 4,
+        DOUT_5 = 1u << 5,
+        DOUT_6 = 1u << 6,
+        DOUT_7 = 1u << 7,
+        DOUT_8 = 1u << 8,
+        DOUT_9 = 1u << 9,
+        DOUT_10 = 1u << 10,
+        DOUT_11 = 1u << 11,
+        DOUT_12 = 1u << 12,
+        DOUT_13 = 1u << 13,
+        DOUT_14 = 1u << 14,
+        DOUT_15 = 1u << 15,
+        DOUT_16 = 1u << 16,
+        DOUT_17 = 1u << 17,
+        DOUT_18 = 1u << 18,
+        DOUT_19 = 1u << 19,
+        DOUT_20 = 1u << 20,
+        DOUT_21 = 1u << 21,
+        DOUT_22 = 1u << 22,
+        DOUT_23 = 1u << 23,
+        DOUT_24 = 1u << 24,
+        DOUT_25 = 1u << 25,
+        DOUT_26 = 1u << 26,
+        DOUT_27 = 1u << 27,
+        DOUT_28 = 1u << 28,
+        DOUT_29 = 1u << 29,
+        DOUT_30 = 1u << 30,
+        DOUT_31 = 1u << 31
+    };
+}
+
 #endif // EC_SLAVE_BASE_DIGITAL_OUT_H
