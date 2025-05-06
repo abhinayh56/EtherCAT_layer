1b6d1e9c12b18aef7414a5bd222449a754674c4f
abhinayh56 <kumarabhinay53@gmail.com>
2025-05-06 17:53:11 +0530

    dfgfdg

--- src/Ec_utils/Ec_utils.h
+++ src/Ec_utils/Ec_utils.h
@@ -44,4 +44,14 @@ uint16_t GET_BIT(T value, uint16_t bit_index)
     }
 }
 
+template <typename T>
+struct Pdo_variable
+{
+    uint16_t index;
+    uint8_t subindex;
+    uint32_t offset;
+    T value;
+    bool is_supported = false;
+};
+
 #endif // EC_UTILS_H
